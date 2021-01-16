84833a88a27b5f7f15b44a89566dc6416e0515b0
empower87 <mjamespower87@gmail.com>
2021-01-15 20:53:57 -0800

    done

--- js/index.js
+++ js/index.js
@@ -1,42 +1,70 @@
-// ITERATION 1
-
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+document.querySelector('#calculate').onclick = function() {
+  let allProducts = document.querySelectorAll('.product')
+  let total = 0
+  for (let i = 0; i < allProducts.length; i++) {
+    let eachProduct = allProducts[i]
+    let price = eachProduct.querySelector('.price span').innerHTML
+    let value = eachProduct.querySelector('.quantity input').value
+    let subTotal = price * value
+    eachProduct.querySelector('.subtotal span').innerHtml = subTotal
+    total += subTotal
+  }
+  document.querySelector('#total-value span').innerText = total
+  console.log(total)
 }
 
-function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
-  // ITERATION 2
-  //... your code goes here
+let selectQuantity = document.querySelectorAll('.product')
 
-  // ITERATION 3
-  //... your code goes here
+for (let i = 0; i < selectQuantity.length; i++) {
+  let ep = selectQuantity[i]
+  let newSub = ep.querySelector('.subtotal span')
+  let input = ep.querySelector('input')
+  let unitPrice = parseInt(ep.querySelector('.price span').innerText)
+  input.oninput = function() {
+    newSub.innerText = unitPrice * input.value
+    console.log(newSub)
+  }
 }
 
-// ITERATION 4
+//   let input = document.querySelector('input')
 
-function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
-}
+  // unitValue.addEventListener('input', function (event) {
+  //   newSub = unitPrice * unitValue
+  // })
+//   input.oninput = function() {
+//     console.log("click")
+//     newSub = unitPrice * input.value
+//     console.log(newSub)
+//   }
+// }
 
-// ITERATION 5
 
-function createProduct() {
-  //... your code goes here
-}
+// let selectQuantity = document.querySelectorAll('.product')
 
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+// selectQuantity.forEach(item => {
+//   item.addEventListener('input', event => {
+//     console.log(item)
+//     let newSub = document.querySelector('.subtotal')
+//     let unitPrice = parseInt(document.querySelector('.price span').innerText)
+//     let input = document.querySelector('input')
+//     newSub = unitPrice * input.value
+//     console.log("click")
+//     console.log(newSub)
+//   })
+// })
 
-  //... your code goes here
-});
+
+// selectQuantity.oninput = function() {
+//   console.log("click")
+
+
+// function wireUpRemove() {
+  // let removeButtons = document.querySelectorAll('.btn-remove.btn')
+  // for (let i =0; i < removeButtons.length; i++) {
+    // let removeButtons = removeButtonsp[i]
+    // removeButtons.onClick = function(event) {
+      // this.parentNode.parentNode.remove()
+    // }
+  // }
+//}
